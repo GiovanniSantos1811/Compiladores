494c410a92df16726d4ad4083eeabce1e3b44d6a
rhayna <rhayna.casado@wr.sc.usp.br>
2025-04-17 13:33:52 -0300

    matriz_transicao

--- Trabalho1/Código_Compiladores/apps/teste.c
+++ Trabalho1/Código_Compiladores/apps/teste.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "hash.h"
 #include "arquivo.h"
-
+#include "automato.h"
 
 // int main() {
 
@@ -23,8 +23,30 @@ int main() {
     const char *nome_saida = "output.txt";
     remove(nome_saida);
 
-    ler_arquivo_caractere(nome_programa);
-    escrever_arquivo(nome_saida, "testando");
+    // ler_arquivo_caractere(nome_programa);
+    // escrever_arquivo(nome_saida, "testando");
 
+    FILE *programa = fopen(nome_programa, "r");
+    if (programa == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return -1;
+    }
+
+    char char_prox = fgetc(programa);
+    char *token_atual;
+    int estado_atual = 0;
+
+    while (char_prox != EOF) {
+        printf("prox caractere: %c\n", char_prox);
+        estado_atual = transicao(char_prox, estado_atual);
+        if(estado_atual < 0){
+
+        } else {
+            // coloca o char_prox no final do token_atual
+
+        }
+    }
+    
+    fclose(programa);
     return 0;
 }
